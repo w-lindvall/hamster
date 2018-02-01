4218c406a750ba7d2cf43b1a60399e88c2d1d830
William L <lindvall.w@icloud.com>
2018-01-31 20:46:02 -0500

    Link index view to index template

--- ideas/views.py
+++ ideas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ideas.models import Idea, Medium
+
+
+def index(request):
+    recent_ideas = Idea.objects.order_by('-created_at')[:5]
+    medium_list = Medium.objects.order_by('name')
+    context_dict = {'recent_ideas': recent_ideas, 'medium_list': medium_list}
+    return render(request, 'ideas/index.html', context_dict)
